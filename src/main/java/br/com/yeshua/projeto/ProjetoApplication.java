f03480100ab1acfbabc5e53616e1ddcef31a3588
Pedro Sena <pedrosbmartinazzo@hotmail.com>
2024-06-13 20:42:10 -0300

    ambiente de desenvolvimento com banco h2

--- src/main/java/br/com/yeshua/projeto/ProjetoApplication.java
+++ src/main/java/br/com/yeshua/projeto/ProjetoApplication.java
@@ -2,9 +2,20 @@ package br.com.yeshua.projeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
 @SpringBootApplication
 public class ProjetoApplication {
+
+	@GetMapping
+	public ResponseEntity<String> status(){
+		return ResponseEntity.ok("Working!");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
