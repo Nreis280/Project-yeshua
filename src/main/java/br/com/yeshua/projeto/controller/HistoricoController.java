e86a3be99badd9341d08b34974311b42ff5c9dfb
pedro <pedrosbmartinazzo@hotmail.com>
2024-06-26 17:48:46 -0300

    mudança em historico

--- src/main/java/br/com/yeshua/projeto/controller/HistoricoController.java
+++ src/main/java/br/com/yeshua/projeto/controller/HistoricoController.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,11 +34,18 @@ public class HistoricoController {
     PagedResourcesAssembler<Historico> pageAssembler;
 
     @GetMapping
-    public PagedModel<EntityModel<Historico>> index(Pageable pageable) {
+    public PagedModel<EntityModel<Historico>> index(@PageableDefault(sort = "data", direction = Direction.DESC) Pageable pageable) {
         Page<Historico> page = null;
 
         page = historicoRepository.findAll(pageable);
 
         return pageAssembler.toModel(page, Historico::toEntityModel);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Historico> historicoById(@PathVariable Long id, Pageable pageable) {
+        Historico historico = historicoRepository.findById(id).get();
+
+        return ResponseEntity.ok(historico);
     }
 }
